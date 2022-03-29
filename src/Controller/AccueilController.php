4306351e4cc4c3948492b2b4d8fc7105ddf15d25
Lempereurjulien <lempereur.julien83@gmail.com>
2022-03-29 14:46:21 +0200

    changement AccueilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AtelierRepository;
 use Doctrine\Persistence\ManagerRegistry;
+
 class AccueilController extends AbstractController{
  
     /**
