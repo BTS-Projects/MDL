fae31a52d9c9c7c62cc8fe465bc8b52fc814e075
GSBProject <v.schalckens@gmail.com>
2022-04-26 12:18:52 +0200

    ajout de l'entity user

---
+++ src/Entity/User.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $numLicence;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mdp;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $confirme;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumLicence(): ?string
+    {
+        return $this->numLicence;
+    }
+
+    public function setNumLicence(string $numLicence): self
+    {
+        $this->numLicence = $numLicence;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getConfirme(): ?bool
+    {
+        return $this->confirme;
+    }
+
+    public function setConfirme(bool $confirme): self
+    {
+        $this->confirme = $confirme;
+
+        return $this;
+    }
+}
