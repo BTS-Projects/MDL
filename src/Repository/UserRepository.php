dcb04dc81eb29d6f8d0e1f0d1efc6230fe70b6ff
Lempereurjulien <lempereur.julien83@gmail.com>
2022-05-01 18:32:56 +0200

    make user "Connexion" avec page login

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +15,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository 
 {
     public function __construct(ManagerRegistry $registry)
     {
