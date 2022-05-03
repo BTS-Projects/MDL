200a458cfbe4aef7859db8b0707353f97d9a836c
GSBProject <v.schalckens@gmail.com>
2022-05-03 11:12:52 +0200

    test

--- src/Form/CreationType.php
+++ src/Form/CreationType.php
@@ -5,13 +5,24 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class CreationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('numeroLicence')
+            ->add('password', RepeatedType::class, [
+                'type'=> PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options'=>['attr'=> ['class' => 'password-field']],
+                'required' => true,
+                'first_options' =>  ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            
+            ] )
         ;
     }
 
