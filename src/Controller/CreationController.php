7e8b8a348c335c86a14238631b970afcc3a892f6
GSBProject <v.schalckens@gmail.com>
2022-05-03 11:05:53 +0200

    avancement inscription

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -20,7 +20,39 @@ class CreationController extends AbstractController
         ]);
     }
     
-//    public function creer(Request $request, EntityManagerInterface $manager) {
-//        $form
-//    }
+    public function register(Request $request, UserPasswordEncoderInterface $userPasswordEncoder, GuardAuthenticatorHandler $guardHandler, ApplicationAuthenticator $authenticator, EntityManagerInterface $entityManager) : Response {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            
+            $user->setRoles(["ROLE_INSCRIT"]);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('admin@navire.sio', 'Admin'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email and Inscription')
+                    ->htmlTemplate('creation/confirmation_email.html.twig')
+            );
+            
+             return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+    }
+                
 }
