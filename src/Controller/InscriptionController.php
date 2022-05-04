d068851440d58e04ec6bfb01dd3c944b2e0dbd02
Lempereurjulien <lempereur.julien83@gmail.com>
2022-05-04 22:50:43 +0200

    Ajout connecté + numéro licence sur page Inscriptions

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -12,6 +12,7 @@ use App\Repository\LicencieRepository;
 use App\Entity\Licencie;
 use App\Entity\Compte;
 use App\Repository\AtelierRepository;
+
 class InscriptionController extends AbstractController
 {
     /**
@@ -19,20 +20,20 @@ class InscriptionController extends AbstractController
      */
     public function Inscription(UserRepository $repo,LicencieRepository $repoLicencie,AtelierRepository $repoAtelier){
         
-//        $licencie = new Licencie;
-//        $lesLicencies = $repoLicencie->findAll();
-//        $user = $repo->findAll();
-//        $i = 0;
-//        while( $i < count($lesLicencies) && $lesLicencies[$i]->getNumlicence() != $user->getNumLicence() ) {
-//            $i++;
-//        }
-//        if ($i < count($lesLicencies)) {
-//            $licencie = $lesLicencies[0];//$i
-//        }
+        $licencie = new Licencie;
+        $lesLicencies = $repoLicencie->findAll();
+        $user = $repo->findAll();
+        $i = 0;
+        while( $i < count($lesLicencies) && $lesLicencies[$i]->getNumlicence() != $user[$i]->getNumLicence() ) {
+            $i++;
+        }
+        if ($i < count($lesLicencies)) {
+            $licencie = $lesLicencies[$i];
+        }
         $lesAteliers =$repoAtelier->findAll();
         $form = $this->createForm(InscriptionType::class);
         return $this->render('inscription/inscription.html.twig',
-                [   //'licencie'=>$licencie,
+                [   'licencie'=>$licencie,
                     'lesAtelier'=>$lesAteliers,
                     'form'=>$form->createView()]);
     }
